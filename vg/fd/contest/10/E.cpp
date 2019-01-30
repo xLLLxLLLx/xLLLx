569ac794569e1813a8d4faa7b861d58ce304d315
xLLLx <pysxlqp@163.com>
2019-01-30 20:05:25 +0800

    add new files

--- vg/fd/contest/10/E.cpp
+++ vg/fd/contest/10/E.cpp
@@ -2,89 +2,49 @@
 #define fr(i,x,y) for(int i=x;i<=y;++i)
 #define rf(i,x,y) for(int i=x;i>=y;--i)
 #define LL long long
-#define db double
 using namespace std;
-const int N=510,M=1e5+10;
-struct data{
-	db w;
-	int rk;
-}a[N];
-int b[M],v[N],qx[N<<1];
-string lz;
-string lx[N],ll[N];
+const int N=25,M=(1<<20)+1;
+char lx[N][N];
+int zt[N][N];
+LL a[N][N],val[N][N],dp[M],c[N][N];
 
-bool cmp(data xx,data yy){
-	if(xx.w!=yy.w) return xx.w>yy.w;
-	return ll[xx.rk]<ll[yy.rk];
+int lowerbit(int x){
+	return x&(-x);
 }
 
-void init(){
-	fr(i,1000,9999){
-		int flg=0,len=sqrt(i);
-		fr(j,2,len){
-			if(i%j==0) {
-				flg=1;break;
-			}
-		}
-		if(!flg) b[i]=1;
-	}
-}
-
-bool comp(int x,int y){
-	return x>y;
+void Min(LL &x,LL y){
+	x=min(x,y);
 }
 
 int main(){
-	int T;scanf("%d",&T);
-	fr(o,1,T){
-		int n,m;scanf("%d%d",&n,&m);
-		memset(b,0,sizeof b),memset(v,0,sizeof v);
-		init();
-		int t,x;scanf("%d",&t);
-
-		fr(i,1,t) scanf("%d",&x),b[x]=2;
-		scanf("%d",&t);
-
-		fr(i,1,t) scanf("%d",&x),b[x]=3;
-		int dw;scanf("%d",&dw);
-
-		fr(i,1,dw) {
-			cin>>lx[i];
-			scanf("%d",&v[i]);
-			if(v[i]==1) v[i]=36;
-			if(v[i]==2) v[i]=27;
-			if(v[i]==3) v[i]=18;
+	int n,m;scanf("%d%d",&n,&m);
+	fr(i,0,n-1) scanf("%s",lx[i]+1);
+	fr(i,0,n-1) fr(j,1,m) scanf("%lld",&a[i][j]);
+	fr(i,0,n-1) fr(j,1,m) {
+		LL maxn=0;
+		fr(k,0,n-1){
+			if(lx[i][j]!=lx[k][j]) continue;
+			zt[i][j]|=(1<<k),val[i][j]+=a[k][j];
+			maxn=max(maxn,a[k][j]);
 		}
-		
-		fr(i,1,n) a[i].w=0,a[i].rk=i;
-
-		fr(i,1,n) {
-			cin>>ll[i];cin>>lz;
-			fr(j,1,dw){
-				if(lz==lx[j]) {
-					a[i].w+=v[j];
-					break;
-				}
-			}
-			char ch;cin>>ch;
-			if(ch=='F') a[i].w+=33;
-			int kk,tt;scanf("%d%d",&kk,&tt);
-			fr(o,1,kk) {
-				int x;scanf("%d",&x);
-				if(b[x]==1) a[i].w+=1.0;
-				else if(b[x]==2) a[i].w+=2.5;
-				else if(b[x]==3) a[i].w+=1.5;
-				else a[i].w+=0.3;
-			}
-			fr(o,1,tt) scanf("%d",&qx[o]);
-			if(tt) sort(qx+1,qx+1+tt,comp);
-			if(tt>=3) a[i].w+=db(db(max(0,(qx[3]-1200)))/100.0)*1.5;
+		if(zt[i][j]!=(1<<i)) c[i][j]=a[i][j];
+		val[i][j]-=maxn;
+		//printf("%d %d %d %lld\n",i,j,zt[i][j],val[i][j]);
+	}
+	int mask=(1<<n)-1;
+	memset(dp,0x3f,sizeof dp);
+	dp[0]=0;
+	fr(o,0,mask-1) {
+		int pos;
+		fr(i,0,n-1) {
+			if(!((o>>i)&1)) {pos=i;break;}
 		}
-		sort(a+1,a+1+n,cmp);
 		fr(i,1,m) {
-			cout<<ll[a[i].rk]<<" ";
-			printf("%.3lf\n",a[i].w);
+			Min(dp[o|zt[pos][i]],dp[o]+val[pos][i]);
+			Min(dp[o|(1<<pos)],dp[o]+c[pos][i]);
 		}
 	}
+	//fr(o,0,mask-1) printf("%d %lld\n",o,dp[o]);
+	printf("%lld\n",dp[mask]);
 	return 0;
 }
--- vg/fd/hld/B.cpp
+++ vg/fd/hld/B.cpp
@@ -1,4 +1,3 @@
-#pragma GCC optimize(2)
 #include<bits/stdc++.h>
 #define fr(i,x,y) for(int i=x;i<=y;++i)
 #define rf(i,x,y) for(int i=x;i>=y;--i)
@@ -6,113 +5,120 @@
 #define lson x<<1
 #define rson x<<1|1
 using namespace std;
-const int N=1e4+10,M=10;
+const int N=3e4+10;
 struct data{
-	int nt,to,w,rk;
-}a[N<<1];
-char lx[M];
-int cnt=0,res=0,n,wtf;
-int v[N<<2],head[N],ys[N],w[N],tp[N],s[N],dep[N],f[N],sz[N],id[N],ed[N];
+	int nt,to;
+}a[N<<2];
+int cnt=0,res=0,n,m;
+int v[N<<2],sum[N<<2];
+int head[N],dep[N],w[N],f[N],sz[N],s[N],id[N],tp[N];
 
-void read(int &x){
-	char ch=getchar();x=0;
-	for(;ch<'0'||ch>'9';ch=getchar()) ;
-	for(;ch>='0'&&ch<='9';ch=getchar()) x=(x<<1)+(x<<3)+(ch^48);
-}
-
-void add(int x,int y,int w,int rk){
-	a[++cnt].to=y,a[cnt].w=w,a[cnt].rk=rk,a[cnt].nt=head[x],head[x]=cnt;
-	a[++cnt].to=x,a[cnt].w=w,a[cnt].rk=rk,a[cnt].nt=head[y],head[y]=cnt;
+void add(int x,int y){
+	a[++cnt].to=y,a[cnt].nt=head[x],head[x]=cnt;
+	a[++cnt].to=x,a[cnt].nt=head[y],head[y]=cnt;
 }
 
 void dfs1(int u,int fa){
-	sz[u]=1,f[u]=fa,dep[u]=dep[fa]+1;
+	dep[u]=dep[fa]+1,f[u]=fa,sz[u]=1;
 	for(int i=head[u];i;i=a[i].nt){
 		int to=a[i].to;
 		if(to==fa) continue;
-		ys[a[i].rk]=to,w[to]=a[i].w,dfs1(to,u);
-		if(sz[s[u]]<sz[to]) s[u]=to;
+		dfs1(to,u);
 		sz[u]+=sz[to];
+		if(sz[to]>sz[s[u]]) s[u]=to;
 	}
 }
 
 void dfs2(int u,int v){
-	id[u]=++res,tp[u]=v;
-	if(!s[u]) {
-		ed[u]=res;
-		return ;
-	}
+	id[u]=++res;tp[u]=v;
+	if(!s[u]) return ;
 	dfs2(s[u],v);
 	for(int i=head[u];i;i=a[i].nt){
-		int to=a[i].to;
-		if(to==f[u]||to==s[u]) continue;
-		dfs2(to,to);
+		if(a[i].to==f[u]||a[i].to==s[u]) continue;
+		dfs2(a[i].to,a[i].to);
 	}
-	ed[u]=res;
 }
 
 void change(int x,int l,int r,int L,int k){
-	if(l==r&&l==L){
-		v[x]=k;
+	if(l==r&&L==l) {
+		v[x]=k;sum[x]=k;
 		return ;
 	}
 	int mid=(l+r)>>1;
 	if(L<=mid) change(lson,l,mid,L,k);
 	else change(rson,mid+1,r,L,k);
 	v[x]=max(v[lson],v[rson]);
+	sum[x]=sum[lson]+sum[rson];
+}
+
+int get_m(int x,int l,int r,int L,int R){
+	if(L<=l&&r<=R) return v[x];
+	int mid=(l+r)>>1,ans1=-6e8,ans2=-6e8;
+	if(L<=mid) ans1=get_m(lson,l,mid,L,R);
+	if(R>mid) ans2=get_m(rson,mid+1,r,L,R);
+	return max(ans1,ans2);
 }
 
 int get(int x,int l,int r,int L,int R){
-	if(L<=l&&r<=R) return v[x];
+	if(L<=l&&r<=R) return sum[x];
 	int mid=(l+r)>>1,ans1=0,ans2=0;
 	if(L<=mid) ans1=get(lson,l,mid,L,R);
-	if(R>mid) ans2+=get(rson,mid+1,r,L,R);
-	return max(ans1,ans2);
+	if(R>mid) ans2=get(rson,mid+1,r,L,R);
+	return (ans1+ans2);
+}
+
+int findm(int x,int y){
+	int fx=tp[x],fy=tp[y];
+	int ans=-6e8;
+	for(;fx!=fy;){
+		if(dep[fx]>dep[fy]){
+			ans=max(ans,get_m(1,1,n,id[fx],id[x]));
+			x=f[fx],fx=tp[x];
+		} else {
+			ans=max(ans,get_m(1,1,n,id[fy],id[y]));
+			y=f[fy],fy=tp[y];
+		}
+	}
+	if(dep[x]>dep[y]) ans=max(ans,get_m(1,1,n,id[y],id[x]));
+	else ans=max(ans,get_m(1,1,n,id[x],id[y]));
+	return ans;
 }
 
 int find(int x,int y){
 	int fx=tp[x],fy=tp[y];
-	int maxn=0;
+	int ans=0;
 	for(;fx!=fy;){
 		if(dep[fx]>dep[fy]) {
-			maxn=max(maxn,get(1,1,n,id[fx],id[x]));
+			ans+=get(1,1,n,id[fx],id[x]);
 			x=f[fx],fx=tp[x];
 		} else {
-			maxn=max(maxn,get(1,1,n,id[fy],id[y]));
+			ans+=get(1,1,n,id[fy],id[y]);
 			y=f[fy],fy=tp[y];
 		}
 	}
-	if(dep[x]>dep[y]) maxn=max(maxn,get(1,1,n,id[y]+1,id[x]));
-	if(dep[x]<dep[y]) maxn=max(maxn,get(1,1,n,id[x]+1,id[y]));
-	return maxn;
+	if(id[x]>=id[y]) ans+=get(1,1,n,id[y],id[x]);
+	else ans+=get(1,1,n,id[x],id[y]);
+	return ans;
 }
 
+char lx[10];
+
 int main(){
-	//freopen("out","r",stdin);
-	int T;read(T);
-	fr(o,1,T){
-		memset(head,0,sizeof head);
-		memset(s,0,sizeof s);
-		cnt=0,res=0;
-		read(n);
-		fr(i,1,n-1) {
-			int x,y,z;
-			read(x),read(y),read(z);
-			add(x,y,z,i);
-		}
-		dfs1(1,1),dfs2(1,1);
-		fr(i,2,n) change(1,1,n,id[i],w[i]);
-		for(;;){
-			int x,y;
-			scanf("%s",lx+1);
-			if(lx[1]=='D') break;
-			read(x),read(y);
-			if(lx[1]=='Q'){
-				printf("%d\n",find(x,y));
-			} else if(lx[1]=='C'){
-				change(1,1,n,id[ys[x]],y);
-			} 
-		}
+	scanf("%d",&n);
+	fr(i,1,n-1){
+		int x,y;
+		scanf("%d%d",&x,&y);
+		add(x,y);
+	}
+	dfs1(1,1),dfs2(1,1);
+	fr(i,1,n) scanf("%d",&w[i]),change(1,1,n,id[i],w[i]);
+	scanf("%d",&m);
+	fr(i,1,m){
+		int x,y;
+		scanf("%s%d%d",lx+1,&x,&y);
+		if(lx[4]=='X') printf("%d\n",findm(x,y));
+		else if(lx[4]=='M') printf("%d\n",find(x,y));
+		else change(1,1,n,id[x],y);
 	}
 	return 0;
 }
