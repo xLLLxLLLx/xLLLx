f81fabca2095e86a0f26ac485a9a8ff61c7847e3
xLLLxLLLx <2656020973@qq.com>
2019-02-10 21:36:18 +0800

    add new files

--- vg/fd/contest/12/C.cpp
+++ vg/fd/contest/12/C.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<map>
+#include<stack>
+#include<vector>
+#include<queue>
+#include<set>
+#include<algorithm>
+
 #define fr(i,x,y) for(int i=x;i<=y;++i)
 #define rf(i,x,y) for(int i=x;i>=y;--i)
 #define LL long long
 using namespace std;
-const int N=2e6+10,M=6e5+10,mod=19260817;
-int ch[N][4],sh[N][4],f[N],cnt=0,size=0,res=0,tr;
-LL b[M];
+const int N=2e6+10,M=6e5+10,mod=1e9+7;
+int ch[N][4],sh[N][4],A[M],b[M],f[N];
+int hc=0,hs=0;
+LL ds=M;
 char lx[M];
-
-LL mul(LL x,LL y){
-	return (x*y)%mod;
-}
-
-LL add(LL x,LL y){
-	return (x+y)%mod;
-}
+map<LL,int>mp;
 
 void Add(LL &x,LL y){
 	x=(x+y)%mod;
@@ -24,55 +28,47 @@ void Mul(LL &x,LL y){
 	x=(x*y)%mod;
 }
 
-void insert(){
-	int len=strlen(lx+1),fa=0;
-	fr(i,1,len) {
-		int pos=lx[i]-'a'+1;
-		if(!ch[fa][pos]) ch[fa][pos]=++cnt;
-		fa=ch[fa][pos];
-	}
-	fa=0;
-	rf(i,len,1) {
-		int pos=lx[i]-'a'+1;
-		if(!sh[fa][pos]) sh[fa][pos]=++size,f[sh[fa][pos]]=fa;
-		fa=sh[fa][pos];
-	}
+LL add(LL x,LL y){
+	return (x+y)%mod;
 }
 
-int find(int len){
-	int fa=0;
-	rf(i,len,2) {
-		if(fa>tr) res++;
-		int pos=lx[i]-'a'+1;
-		if(!sh[fa][pos]) sh[fa][pos]=++size,f[sh[fa][pos]]=fa;
-		fa=sh[fa][pos];
+LL mul(LL x,LL y){
+	return (x*y)%mod;
+}
+
+void push(){
+	LL zz=a[0],pos=ds;
+	fr(i,1,2) {
+		Add(zz,mul(a[i],pos));
+		Mul(pos,pos);
 	}
-	return fa;
+	mp[zz]=1;
+}
+
+void insert(){
+	int fa=0,len=strlen(lx+1);
+	fr(i,1,len) {
+		int pos=lx[i]-'a'+1;
+		if(!ch[fa][pos]) ch[fa][pos]=++hc;
+		A[i]=fa=ch[fa][pos];
+	}
+	rf(i,len,1) {
+		int pos=lx[i]-'a'+1;
+		if(!sh[fa][pos]) sh[fa][pos]=++hs;
+		f[ch[fa][pos]]=fa;
+		B[i]=fa=sh[fa][pos];
+	}
+	A[0]=0;B[len+1]=0;
+	fr(i,0,len-1) push(A[i],B[i+2]);
 }
 
 int main(){
 	int n,m;scanf("%d%d",&n,&m);
-	fr(i,1,n) {
+	fr(i,1,n) scanf("%s",lx+1),insert();
+	fr(i,1,m) {
 		scanf("%s",lx+1);
-		insert();
-	}
-	tr=size;
-	fr(i,1,m){
-		scanf("%s",lx+1);
-		int len=strlen(lx+1),flg=0;
-		res=0;
-		int tmp=find(len);
-		if(!res) {printf("YES\n");continue;}
-		int nw=0;
-		fr(j,2,len){
-			int pos=lx[j-1]-'a'+1;
-			if(!ch[nw][pos]) break;
-			nw=ch[nw][pos];
-			if(tmp>tr) res--;
-			tmp=f[tmp];
-			if(!res) {printf("%d\n",j),printf("YES\n");flg=1;break;}
-		}
-		if(!flg) printf("NO\n");
+		int x=0,y=Get();
+		
 	}
 	return 0;
 }
