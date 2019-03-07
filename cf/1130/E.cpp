a8f98d88275e862848b276a93ca25545aa379fb0
xjoi <pysxlqp@163.com>
2019-03-07 22:07:46 +0800

    add new files

--- cf/1130/E.cpp
+++ cf/1130/E.cpp
@@ -8,6 +8,18 @@ int a[N];
 
 int main(){
 	int k;scanf("%d",&k);
-	
+	LL sum=k+2000;
+	cout<<"2000"<<endl;
+	cout<<"-1 ";
+	fr(i,2,2000){
+		if(!sum) cout<<"0 ";
+		else if(sum>=1e6) {
+			cout<<"1000000 ";
+			sum-=1e6;
+		} else {
+			cout<<sum<<" ";
+			sum=0;
+		}
+	}
 	return 0;
 }
